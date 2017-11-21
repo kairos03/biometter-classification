e734b831b4328c081ab7b6a1b21a3d45d54ac5a7
karios03 <kairos9603@gmail.com>
2017-11-21 14:31:06 +0100

    fix all bug in model-es

--- model-es.py
+++ model-es.py
@@ -6,9 +6,11 @@ from data import input_data
 import numpy as np
 
 learning_rate = 1e-3
-epoch = 100
+total_epoch = 100
 batch_size = 30
 dropout_keep_prob = 0.9
+
+log_root = './log/lr_{}_{}/'.format(learning_rate, time.time())
 
 train_data, test_data = input_data.read_train_and_test_data()
 
@@ -21,8 +23,8 @@ def one_hot(arr, depth):
 
 def next_batch(batch, is_train=True):
     if is_train:
-        xs = train_data['image'][:(batch+1)*batch_size]
-        ys = train_data['is_contacted'][:(batch+1)*batch_size]
+        xs = train_data['image'][:(batch + 1) * batch_size]
+        ys = train_data['is_contacted'][:(batch + 1) * batch_size]
 
     else:
         xs = train_data['image'][:]
@@ -36,11 +38,11 @@ def next_batch(batch, is_train=True):
 
 
 def train():
-
     with tf.name_scope('input'):
         X = tf.placeholder(tf.float32, [None, 640, 512, 2])
         Y = tf.placeholder(tf.float32, [None, 2])
         keep_prob = tf.placeholder(tf.float32)
+
     #
     # with tf.name_scope('input_image'):
     #     tf.summary.image('input', x, 10)
@@ -71,25 +73,25 @@ def train():
 
     with tf.name_scope('conv1'):
         filter1 = var_weight([10, 8, 2, 16])
-        conv1 = tf.nn.conv2d(X, filter1, strides=[1, 1, 1, 1], padding='SAME')
+        conv1 = tf.nn.conv2d(X, filter1, strides=[1, 10, 8, 1], padding='SAME')
         conv1 = tf.nn.relu(conv1)
         tf.summary.histogram('act', conv1)
 
     with tf.name_scope('max_pool1'):
-        pool1 = tf.nn.max_pool(conv1, [1, 2, 2, 1], [1, 1, 1, 1], padding='SAME')
+        pool1 = tf.nn.max_pool(conv1, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME')
 
     with tf.name_scope('conv2'):
         filter2 = var_weight([2, 2, 16, 32])
-        conv2 = tf.nn.conv2d(pool1, filter2, strides=[1, 1, 1, 1], padding='SAME')
+        conv2 = tf.nn.conv2d(pool1, filter2, strides=[1, 2, 2, 1], padding='SAME')
         conv2 = tf.nn.relu(conv2)
         tf.summary.histogram('act', conv2)
 
     with tf.name_scope('max_pool2'):
-        pool2 = tf.nn.max_pool(conv2, [1, 2, 2, 1], [1, 1, 1, 1], padding='SAME')
+        pool2 = tf.nn.max_pool(conv2, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME')
 
     with tf.name_scope('conv3'):
         filter3 = var_weight([2, 2, 32, 64])
-        conv3 = tf.nn.conv2d(pool2, filter3, strides=[1, 1, 1, 1], padding='SAME')
+        conv3 = tf.nn.conv2d(pool2, filter3, strides=[1, 2, 2, 1], padding='SAME')
         conv3 = tf.nn.relu(conv3)
         tf.summary.histogram('act', conv3)
 
@@ -97,7 +99,7 @@ def train():
     #     pool2 = tf.nn.max_pool(conv3, [1, 2, 2, 1], [1, 1, 1, 1], padding='SAME')
 
     with tf.name_scope('reshape'):
-        reshaped = tf.reshape(conv3, [None, 4 * 4 * 64])
+        reshaped = tf.reshape(conv3, [-1, 4 * 4 * 64])
 
     with tf.name_scope('fc1'):
         weight1 = var_weight([4 * 4 * 64, 512])
@@ -111,42 +113,59 @@ def train():
 
     with tf.name_scope('fc2'):
         weight2 = var_weight([512, 2])
-        bias2 = var_bias([2])
-        model = tf.matmul(fc1, weight2) + bias2
+        # bias2 = var_bias([2])
+        model = tf.matmul(fc1, weight2)  # + bias2
 
     #
     with tf.name_scope('matrix'):
         cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=model, labels=Y))
-        optmizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+        optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+        accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(model, 1), tf.argmax(Y, 1)), tf.float32))
 
-    train_log = tf.summary.FileWriter('./log/train/lr_{}_{}'.format(learning_rate, time.time()))
+        tf.summary.scalar('accuracy', accuracy)
+        tf.summary.scalar('xent', cost)
+
+    train_log = tf.summary.FileWriter(log_root+'train'.format(learning_rate, time.time()))
+    test_log = tf.summary.FileWriter(log_root+'test'.format(learning_rate, time.time()))
     merged = tf.summary.merge_all()
 
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
 
         total_batch = len(train_data['image']) // batch_size
-        for i in range(epoch):
+
+        print('Train Start')
+        for epoch in range(total_epoch):
+            total_loss = 0
+            summary = None
+
             for batch in range(total_batch):
                 xs, ys = next_batch(batch)
 
-                summary, _ = sess.run([merged, optmizer],
-                                      feed_dict={
-                                          X: xs,
-                                          Y: ys,
-                                          keep_prob: dropout_keep_prob})
-                if (epoch == 0 or epoch % 10 == 9) and batch == 0:
-                    train_log.add_summary(summary, epoch)
-                # else:
-                #     sess.run([optmizer],
-                #              feed_dict={
-                #                  x: xs,
-                #                  y_: ys,
-                #                  keep_prob: keep_prob})
+                summary, loss, _ = sess.run([merged, cost, optimizer],
+                                            feed_dict={
+                                                X: xs,
+                                                Y: ys,
+                                                keep_prob: dropout_keep_prob})
+                total_loss += loss
 
-            # if epoch != 0 or epoch % 10 == 0:
-            #     xs, ys = next_batch(batch, False)
+            train_log.add_summary(summary, epoch)
+            print('epoch: {}, loss: {}'.format(epoch, total_loss / total_batch))
 
+            if epoch % 5 == 0:
+                xs, ys = next_batch(batch, False)
+
+                summary, acc = sess.run([merged, accuracy],
+                                        feed_dict={
+                                            X: xs,
+                                            Y: ys,
+                                            keep_prob: 1})
+
+                test_log.add_summary(summary, epoch)
+                print('accuracy: {}'.format(acc))
+
+    print('Finish')
     train_log.close()
+
 
 train()
